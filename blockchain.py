25819497fb08b07fa29428c49ae1ecbfea656801
Ryan <ryan.dsilva.98@gmail.com>
2018-09-13 15:56:20 +0530

    Added BlockChain and Transaction Class

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,13 @@ import hashlib
 from datetime import datetime as dt
 
 
+class Transaction:
+    def __init__(self, toAddress, fromAddress, amount):
+        self.toAddress = toAddress
+        self.fromAddress = fromAddress
+        self.amount = amount
+
+
 class Block:
     def __init__(self, transaction, nonce, previousHash):
         self.createdAt = dt.now()
@@ -15,3 +22,25 @@ class Block:
             str(self.transaction) + str(self.createdAt)
         hashString = hashString.encode()
         return hashlib.sha256(hashString).hexdigest()
+
+
+class BlockChain:
+    def __init__(self, name, proofOfWork):
+        self.name = name
+        self.blocks = []
+        self.PROOF_OF_WORK = proofOfWork
+
+    def getLastestBlock(self):
+        return self.blocks[len(self.blocks) - 1]
+
+    def generateGenesisBlock(self):
+        pass
+
+    def validateChain(self):
+        pass
+
+    def addBlock(self):
+        pass
+
+    def proofOfWork(self):
+        pass
