562b6e74488fcb0c03552a909e5ebc340ae12e89
Ryan <ryan.dsilva.98@gmail.com>
2018-09-13 15:49:38 +0530

    Added Block Class

--- blockchain.py
+++ blockchain.py
@@ -1,2 +1,17 @@
 import hashlib
-import datetime
+from datetime import datetime as dt
+
+
+class Block:
+    def __init__(self, transaction, nonce, previousHash):
+        self.createdAt = dt.now()
+        self.transaction = transaction
+        self.nonce = nonce
+        self.previousHash = previousHash
+        self.currentHash = self.calculateHash()
+
+    def calculateHash(self):
+        hashString = str(self.createdAt) + str(self.nonce) + \
+            str(self.transaction) + str(self.createdAt)
+        hashString = hashString.encode()
+        return hashlib.sha256(hashString).hexdigest()
