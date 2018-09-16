07f844d0cbb2c696c31ed4b96787431716d0b219
Ryan <ryan.dsilva.98@gmail.com>
2018-09-16 14:47:42 +0530

    Added Vue Setup

---
+++ webapp/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return jsonify('Home Page Reached!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
